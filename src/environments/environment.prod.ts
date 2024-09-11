838201091fa7041fc92130a9f42d72c686ae22f4
SebastianMiranda <pedro.tellez@clubalpha.com.mx>
2024-09-10 18:36:15 -0600

    pruebas emma

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl:'https://pagos.clubalpha.com.mx:9443/',
+  //apiUrl:'https://pagos.clubalpha.com.mx:9443/',
+  apiUrl: 'https://xkxhds7f-9443.usw3.devtunnels.ms/',
   //vip-esp deportistas
   invitados: 'deportistas',
 };
